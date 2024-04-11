91ebe721d5a31470586a03752cc0daa527257a66
Anthony_Muscarella <muscarellaanthony1@gmail.com>
2024-04-11 16:49:38 -0400

    created card function is funtional

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ function formSubmit(){
     const taskArray = JSON.parse(localStorage.getItem('strings')) || [];
 
     const taskObject = {
+        //add generate task id function
         objectTitle: title.value,
         objectDate: date.value,
         objectDescription: description.value.trim()
@@ -15,12 +16,74 @@ function formSubmit(){
     localStorage.setItem('tasks', JSON.stringify(taskArray))
     console.log(taskArray)
 
+    $('#todo-cards').append(createTaskCard(taskObject));
 }
 
 
-function generateCard(){
+// Todo: create a function to create a task card
+function createTaskCard(task) {
+    console.log(task)
+    const taskCard = $('<div>')
+      .addClass('card project-card draggable my-3')
+    //   .attr('data-project-id', task.id);
+    const cardHeader = $('<div>').addClass('card-header h4').text(task.objectTitle);
+    console.log(task.objectTitle)
+    const cardBody = $('<div>').addClass('card-body');
+    const cardDescription = $('<p>').addClass('card-text').text(task.objectDescription);
+    const cardDueDate = $('<p>').addClass('card-text').text(task.objectDate);
+    const cardDeleteBtn = $('<button>')
+      .addClass('btn btn-danger delete')
+      .text('Delete')
+      .attr('data-task-id', task.id);
+    cardDeleteBtn.on('click', handleDeleteTask);
 
+    //   ? Sets the card background color based on due date. Only apply the styles if the dueDate exists and the status is not done.
+//   if (task.objectDate && project.status !== 'done') {
+    const now = dayjs();
+    const taskDueDate = dayjs(task.objectDate, 'DD/MM/YYYY');
+
+    // ? If the task is due today, make the card yellow. If it is overdue, make it red.
+    if (now.isSame(task.objectDate, 'day')) {
+      taskCard.addClass('bg-warning text-white');
+    } else if (now.isAfter(task.objectDate)) {
+      taskCard.addClass('bg-danger text-white');
+      cardDeleteBtn.addClass('border-light');
+    }
+
+
+  // ? Gather all the elements created above and append them to the correct elements.
+  cardBody.append(cardDescription, cardDueDate, cardDeleteBtn);
+  taskCard.append(cardHeader, cardBody);
+
+  // ? Return the card so it can be appended to the correct lane.
+  console.log('made it')
+  return taskCard;
 }
+
+
+  // ? Use JQuery UI to make task cards draggable
+  $('.draggable').draggable({
+    opacity: 0.7,
+    zIndex: 100,
+    // ? This is the function that creates the clone of the card that is dragged. This is purely visual and does not affect the data.
+    helper: function (e) {
+      // ? Check if the target of the drag event is the card itself or a child element. If it is the card itself, clone it, otherwise find the parent card  that is draggable and clone that.
+      const original = $(e.target).hasClass('ui-draggable')
+        ? $(e.target)
+        : $(e.target).closest('.ui-draggable');
+      // ? Return the clone with the width set to the width of the original card. This is so the clone does not take up the entire width of the lane. This is to also fix a visual bug where the card shrinks as it's dragged to the right.
+      return original.clone().css({
+        width: original.outerWidth(),
+      });
+    },
+  });
+
+  $('.lane').droppable({
+    accept: '.draggable',
+    drop: handleDrop,
+  });
+
+
 
 
 
@@ -32,43 +95,39 @@ const formButton = document.getElementById('formButton')
 formButton.addEventListener('click', formSubmit)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
+    //  nextid != null then nextid++
+    }
 
-}
 
-// Todo: create a function to create a task card
-function createTaskCard(task) {
 
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
